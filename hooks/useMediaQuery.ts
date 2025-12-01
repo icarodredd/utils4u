831a2326a3c7337528c9013c6bcde9d4273e84e6
icarodredd <icarog854@gmail.com>
2025-12-01 15:29:30 -0300

    fix: hydratation errror

--- hooks/useMediaQuery.ts
+++ hooks/useMediaQuery.ts
@@ -1,15 +1,11 @@
 import { useState, useEffect } from "react";
 
-export function useMediaQuery(query: string): boolean {
-  const [matches, setMatches] = useState(() => {
-    if (typeof window !== "undefined") {
-      return window.matchMedia(query).matches;
-    }
-    return false;
-  });
+export function useMediaQuery(query: string): boolean | undefined {
+  const [matches, setMatches] = useState<boolean | undefined>(undefined);
 
   useEffect(() => {
     const media = window.matchMedia(query);
+    setMatches(media.matches);
 
     const listener = (e: MediaQueryListEvent) => setMatches(e.matches);
     media.addEventListener("change", listener);
