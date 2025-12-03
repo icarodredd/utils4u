9ca045ae91495c859535c0f9e77f13b22e695b95
icarodredd <icarog854@gmail.com>
2025-12-03 15:35:34 -0300

    feat: demo popular tools

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
+import { BadgeCheckIcon, ChevronRightIcon } from "lucide-react";
+
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+
 export default function Home() {
   return (
     <main className="mt-4">
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-extrabold tracking-tight first:mt-0 text-center">
+      <h2 className="scroll-m-20 pb-4 text-3xl font-extrabold tracking-tight first:mt-0 text-center">
         Ferramentas Populares
       </h2>
+      <div className="flex flex-col gap-4 items-center">
+        {[1, 2, 3].map((item) => (
+          <Item
+            className="w-5/6"
+            key={item}
+            variant="outline"
+            size="sm"
+            asChild
+          >
+            <a href="#">
+              <ItemMedia>
+                <BadgeCheckIcon className="size-5" />
+              </ItemMedia>
+              <ItemContent>
+                <ItemTitle>Gerador {item}</ItemTitle>
+              </ItemContent>
+              <ItemActions>
+                <ChevronRightIcon className="size-4" />
+              </ItemActions>
+            </a>
+          </Item>
+        ))}
+      </div>
     </main>
   );
 }
--- components/ToolCard.tsx
+++ components/ToolCard.tsx
@@ -9,17 +9,19 @@ import {
 } from "@/components/ui/card";
 
 export default function ToolCard() {
-  <Card>
-    <CardHeader>
-      <CardTitle>Card Title</CardTitle>
-      <CardDescription>Card Description</CardDescription>
-      <CardAction>Card Action</CardAction>
-    </CardHeader>
-    <CardContent>
-      <p>Card Content</p>
-    </CardContent>
-    <CardFooter>
-      <p>Card Footer</p>
-    </CardFooter>
-  </Card>;
+  return (
+    <Card className="w-3/6">
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+        <CardAction>Card Action</CardAction>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
 }
