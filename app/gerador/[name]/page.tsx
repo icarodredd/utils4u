8fbd406e97368371dbe3e37e68d8d955e7cffa68
icarodredd <icarog854@gmail.com>
2025-12-08 17:11:21 -0300

    feat: copy generated values

--- app/gerador/[name]/page.tsx
+++ app/gerador/[name]/page.tsx
@@ -7,6 +7,7 @@ import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { useState } from "react";
 import { formatCPF, formatCNPJ } from "@brazilian-utils/brazilian-utils";
+import { Copy } from "lucide-react";
 
 export default function GenerateData() {
   const params = useParams<{ name: string }>();
@@ -37,12 +38,18 @@ export default function GenerateData() {
       <h2 className="scroll-m-20 pb-4 text-3xl font-extrabold tracking-tight first:mt-0 text-center">
         Gerador de {params?.name.toUpperCase()}
       </h2>
-      <Input
-        className="w-4/6"
-        readOnly
-        value={generatedData}
-        placeholder={params?.name.toUpperCase()}
-      />
+      <div className="w-full flex justify-center gap-4">
+        <Input
+          className="w-4/6"
+          readOnly
+          value={generatedData}
+          placeholder={params?.name.toUpperCase()}
+        />
+        <Button onClick={() => navigator.clipboard.writeText(generatedData)}>
+          <Copy />
+        </Button>
+      </div>
+
       <div className="flex gap-4 w-4/6 justify-center">
         <small className="text-sm leading-none font-medium">
           Gerar com pontuação?
